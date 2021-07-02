bfcd3d2e97c12593ebc36e8bb4832708a0e18802
greatstudentkaze <dfyz0r@list.ru>
2021-07-02 20:26:18 +0300

    feat(pages/courses/[alias].tsx): getStaticPaths use

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,36 @@
+export interface ProductCharacteristic {
+  value: string,
+  name: string,
+}
+
+export interface ReviewModel {
+  name: string,
+  title: string,
+  description: string,
+  rating: number,
+  createdAt: Date,
+  _id: string,
+  __v: number,
+}
+
+export interface ProductModel {
+  _id: string,
+  categories: string[],
+  tags: string[],
+  title: string,
+  link: string,
+  price: number,
+  credit: number,
+  oldPrice: number,
+  description: string,
+  characteristics: ProductCharacteristic[],
+  createdAt: Date,
+  updatedAt: Date,
+  __v: number,
+  image: string,
+  initialRating: number,
+  reviews: ReviewModel[],
+  reviewCount: number,
+  reviewAvg?: number,
+  advantages: string,
+}
