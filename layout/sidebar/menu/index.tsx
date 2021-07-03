8284cfc7bc94976329ba89c7e32abd1151dea63f
greatstudentkaze <dfyz0r@list.ru>
2021-07-03 14:35:31 +0300

    feat(menu): add conditional active classes and menu opening

--- layout/sidebar/menu/index.tsx
+++ layout/sidebar/menu/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import cn from 'classnames';
 
 import { AppContext } from '../../../context/app.context';
@@ -21,6 +22,23 @@ const firstLevelMenu: FirstLevelMenuItem[] = [
 
 const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
+  const router = useRouter();
+
+  const openSecondLevelItemBlock = (secondCategory: string) => {
+    if (!setMenu) {
+      return;
+    }
+
+    const newMenu = menu.map(item => {
+      if (item._id.secondCategory === secondCategory) {
+        item.isOpened = !item.isOpened;
+      }
+
+      return item;
+    });
+
+    setMenu(newMenu);
+  };
 
   const buildFirstLevel = () => {
     return (
@@ -45,31 +63,45 @@ const Menu = (): JSX.Element => {
   const buildSecondLevel = (route: string) => {
     return (
       <ul>
-        {menu.map(item => (
-          <li className={styles.secondLevelItem} key={item._id.secondCategory}>
-            <button type="button">{item._id.secondCategory}</button>
-            <ul className={cn(styles.secondLevelItemBlock, {
-              [styles.secondLevelItemBlockOpened]: item.isOpened,
-            })}>
-              {buildThirdLevel(item.pages, route)}
-            </ul>
-          </li>
-        ))}
+        {menu.map(item => {
+          const pageAliases = item.pages.map(page => page.alias);
+          const thirdCategory = router.asPath.split('/')[2];
+          if (pageAliases.includes(thirdCategory)) {
+            item.isOpened = true;
+          }
+
+          return (
+            <li className={styles.secondLevelItem} key={item._id.secondCategory}>
+              <button type="button" onClick={() => openSecondLevelItemBlock(item._id.secondCategory)}>
+                {item._id.secondCategory}
+              </button>
+              <ul className={cn(styles.secondLevelItemBlock, {
+                [styles.secondLevelItemBlockOpened]: item.isOpened,
+              })}>
+                {buildThirdLevel(item.pages, route)}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
     );
   };
 
   const buildThirdLevel = (pages: PageItem[], route: string) => {
     return (
-      pages.map(page => <li key={page._id} className={cn(styles.thirdLevelItem, {
-        [styles.thirdLevelItemActive]: false,
-        // todo: remove false
-      })}>
-        <Link href={`/${route}/${page.alias}`}>
-          <a>{page.category}</a>
-        </Link>
-      </li>
-      )
+      pages.map(page => {
+        const path = `/${route}/${page.alias}`;
+
+        return (
+          <li key={page._id} className={cn(styles.thirdLevelItem, {
+            [styles.thirdLevelItemActive]: path === router.asPath,
+          })}>
+            <Link href={path}>
+              <a>{page.category}</a>
+            </Link>
+          </li>
+        );
+      })
     );
   };
 
