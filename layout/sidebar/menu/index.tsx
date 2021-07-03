35e4c95e3b883802b752fb5179e878e0173ddc0b
greatstudentkaze <dfyz0r@list.ru>
2021-07-03 14:56:42 +0300

    feat(menu): move firstLevelMenu to helpers

--- layout/sidebar/menu/index.tsx
+++ layout/sidebar/menu/index.tsx
@@ -4,21 +4,10 @@ import { useRouter } from 'next/router';
 import cn from 'classnames';
 
 import { AppContext } from '../../../context/app.context';
-import { FirstLevelMenuItem, PageItem } from '../../../interfaces/menu.interface';
-import { TopLevelCategory } from '../../../interfaces/page.interface';
+import { PageItem } from '../../../interfaces/menu.interface';
+import { firstLevelMenu } from '../../../helpers';
 
 import styles from './index.module.css';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-  { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
 
 const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
