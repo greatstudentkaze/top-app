905f8f289c3660c182db103f4716a194bc6ccb31
greatstudentkaze <dfyz0r@list.ru>
2021-07-03 15:29:49 +0300

    feat(sidebar): add sidebar basic styles

--- layout/sidebar/index.tsx
+++ layout/sidebar/index.tsx
@@ -5,10 +5,13 @@ import Menu from './menu';
 
 import { SidebarProps } from './index.props';
 import styles from './index.module.css';
+import Logo from '../logo.svg';
 
-const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      search
       <Menu />
     </div>
   );
