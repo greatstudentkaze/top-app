e71537163ddfc0255b14a11d450ba1b0b6a69948
greatstudentkaze <dfyz0r@list.ru>
2021-07-04 15:48:46 +0300

    feat(top-page): add components to the TopPage

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,9 @@
+export { default as Advantages } from './advantages';
 export { default as Button } from './button';
 export { default as Card } from './card';
 export { default as HeadingTag } from './heading-tag';
 export { default as HeadHunterData } from './headhunter-data';
 export { default as Paragraph } from './paragraph';
 export { default as Rating } from './rating';
+export { default as Skills } from './skills';
 export { default as Tag } from './tag';
--- page-components/top-page/index.tsx
+++ page-components/top-page/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import { HeadHunterData, HeadingTag, Tag } from '../../components';
+import { Advantages, HeadHunterData, HeadingTag, Skills, Tag } from '../../components';
 
 import { TopPageComponentProps } from './index.props';
 import styles from './index.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
-import Advantages from '../../components/advantages';
 
 const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
   return (
@@ -20,15 +19,18 @@ const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentPro
         {products.length && products.map(product => <li key={product._id}>{product.title}</li>)}
       </ul>
 
-      <div className={styles.vacancies}>
-        <div className={styles.titleWithTag}>
-          <HeadingTag level="2">Вакансии - {page.category}</HeadingTag>
-          <Tag color="red" size="small">hh.ru</Tag>
-        </div>
-        {firstCategory === TopLevelCategory.Courses && <HeadHunterData {...page.hh} />}
+      <div className={styles.titleWithTag}>
+        <HeadingTag level="2">Вакансии - {page.category}</HeadingTag>
+        <Tag color="red" size="small">hh.ru</Tag>
       </div>
 
-      {page.advantages.length > 0 && <Advantages advantages={page.advantages} />}
+      {firstCategory === TopLevelCategory.Courses && page.hh && <HeadHunterData {...page.hh} />}
+
+      {page.advantages && page.advantages.length > 0 && <Advantages advantages={page.advantages} />}
+
+      {page.seoText && <div className={styles.seoText} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+
+      {page.tags.length > 0 && <Skills skills={page.tags} />}
     </>
   );
 };
