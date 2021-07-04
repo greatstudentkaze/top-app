f1fa57010f77bbeca7fb684f4a8d471e6e9d68c1
greatstudentkaze <dfyz0r@list.ru>
2021-07-04 13:48:11 +0300

    feat(top-page): add some styles for TopPageComponent

--- page-components/top-page/index.tsx
+++ page-components/top-page/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { TopPageComponentProps } from './index.props';
 
-const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps) => {
+import { HeadHunterData, HeadingTag, Tag } from '../../components';
+
+import { TopPageComponentProps } from './index.props';
+import styles from './index.module.css';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+
+const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
   return (
     <>
-      {products && products.length}
+      <header className={styles.header}>
+        <HeadingTag level="1">{page.title}</HeadingTag>
+        {products.length && <Tag color="gray" size="medium">{products.length}</Tag>}
+        <div className={styles.sort}>Сортировка</div>
+      </header>
+
+      <ul>
+        {products.length && products.map(product => <li key={product._id}>{product.title}</li>)}
+      </ul>
+
+      <div className={styles.vacancies}>
+        <div className={styles.titleWithTag}>
+          <HeadingTag level="2">Вакансии - {page.category}</HeadingTag>
+          <Tag color="red" size="small">hh.ru</Tag>
+        </div>
+        {firstCategory === TopLevelCategory.Courses && <HeadHunterData {...page.hh} />}
+      </div>
     </>
   );
 };
