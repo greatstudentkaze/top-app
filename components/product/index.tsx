9f4c722a1af3ba37a65ecf1dba9965440be6b67c
greatstudentkaze <dfyz0r@list.ru>
2021-07-09 17:48:30 +0300

    feat(components): add  Product component's styles and markup

--- components/product/index.tsx
+++ components/product/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import cn from 'classnames'
+import cn from 'classnames';
 
-import { Button, Card, Rating, Tag } from '../index';
+import { Button, Card, HeadingTag, Rating, Tag } from '../index';
 
 import { ProductProps } from './index.props';
 import styles from './index.module.css';
+import { formatRuPrice, declOfNum } from '../../helpers';
 
 const Product = ({ data, className, ...props }: ProductProps): JSX.Element => {
   const {
     advantages,
     disadvantages,
     categories,
+    characteristics,
     description,
     price,
     credit,
@@ -28,35 +30,54 @@ const Product = ({ data, className, ...props }: ProductProps): JSX.Element => {
       <div className={styles.logo}>
         <img src={process.env.NEXT_PUBLIC_DOMAIN + image} width="70" height="70" alt="" aria-labelledby={id} />
       </div>
-      <div className={styles.title} id={id}>{title}</div>
-      <div className={styles.price}>{price}</div>
-      <div className={styles.credit}>{credit}</div>
+      <div className={styles.title} id={id}>
+        <HeadingTag level="3">{title}</HeadingTag>
+      </div>
+      <div className={styles.price}>
+        {formatRuPrice(price)}
+        {oldPrice && <Tag className={styles.discount} color="green" size="small">{formatRuPrice(price - oldPrice)}</Tag>}
+      </div>
+      <div className={styles.credit}>
+        {credit}<span>/мес</span>
+      </div>
       <div className={styles.rating}>
         <Rating rating={reviewAvg ?? initialRating} />
       </div>
-      <ul className={styles.tags}>
+      <ul className={styles.categories}>
         {categories.map(category => <li key={category}>
           <Tag color="ghost">{category}</Tag>
         </li>)}
       </ul>
       <div className={styles.priceLabel}>цена</div>
       <div className={styles.creditLabel}>кредит</div>
-      <div className={styles.rateLabel}>{reviewCount} отзывов</div>
+      <div className={styles.rateLabel}>{reviewCount} {declOfNum(reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 
       <hr/>
 
       <p className={styles.description}>{description}</p>
-      <div className={styles.features}>features</div>
-      <div className={styles.advantages}>
-        {advantages}
+      <div className={styles.features}>
+        {characteristics.map(characteristic => (
+          <div className={styles.feature} key={characteristic.name}>
+            <span className={styles.featureLabel}>{characteristic.name}</span>
+            <span className={styles.dots} />
+            <span className={styles.featureValue}>{characteristic.value}</span>
+          </div>
+        ))}
       </div>
-      <div className={styles.disadvantages}>{disadvantages}</div>
+      {advantages && <div className={styles.advantages}>
+        <p className={styles.subtitle}>Преимущества</p>
+        {advantages}
+      </div>}
+      {disadvantages && <div className={styles.disadvantages}>
+        <p className={styles.subtitle}>Недостатки</p>
+        {disadvantages}
+      </div>}
 
       <hr/>
 
       <div className={styles.actions}>
         <Button>Узнать подробнее</Button>
-        <Button appearance="ghost" arrowDirection={'right'}>Читать отзывы</Button>
+        <Button className={styles.reviewsButton} appearance="ghost" arrowDirection={'right'}>Читать отзывы</Button>
       </div>
     </Card>
   );
